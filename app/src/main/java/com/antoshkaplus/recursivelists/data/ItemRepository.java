47b8c1a5a0ea8175ed0bd53e081a7636588532b7
Anton Logunov <antoshkaplus@gmail.com>
2017-04-25 16:32:56 -0400

    ItemRepository impl. notes

--- app/src/main/java/com/antoshkaplus/recursivelists/data/ItemRepository.java
+++ app/src/main/java/com/antoshkaplus/recursivelists/data/ItemRepository.java
@@ -21,17 +21,56 @@ import static com.antoshkaplus.recursivelists.data.Util.*;
  * Created by antoshkaplus on 4/22/17.
  */
 
+// before class can be used should get rootId from the internet
 public class ItemRepository {
-
+	
+	class VariantItem {
+		Item item;
+		Task task;
+		
+		ItemKind kind;
+		
+		private VariantItem() {}
+		
+		Item get() {
+			switch (kind):
+				Item: return item;
+				Task: return task;
+		}
+		
+		static VariantItem create(Item item) {
+			VariantItem v = new VariantItem();
+			v.kind = item.kind;
+			switch (item.kind): {
+				Item: { 
+					v.item = item;
+					break;
+				}
+				Task: {
+					v.task = (Task)item;
+					break;
+				}
+			}
+			return v;
+		}
+	};
+	
+	
     ItemDbRepository dbRepo;
     ItemsApiFactory itemsApiFactory;
     Context context;
-
+	
+	// handles everything
+	HandlerThread worker;
+	Handler taskHandler;
+	
     public ItemRepository(Context ctx, String account, ItemsApiFactory itemsApiFactory) {
         dbRepo = new ItemDbRepository(ctx, account);
         this.itemsApiFactory = itemsApiFactory;
     }
-
+	
+	// at some point we would need to quit worker thread
+	
     public void addNewItem(Item item, Handler handler) {
         // try to push outside first
         // if was able to push outside
@@ -39,26 +78,18 @@ public class ItemRepository {
 
         // after each write we should call sync procedure
         if (Util.isInternetAvailable()) {
-
+			
             Runnable r = new Runnable() {
-
-
-                // do something with the handler on top
-            if (!addNewItemOnline(item, handler)) {
-                // send user that was unable to add item for some reason
-            } else {
-                sync();
-
-            }
-            }
-
-
-            HandlerThread t;
-            t.run();
-
-            Handler hh = new Handler(t.getLooper());
-            hh.post(r);
-
+				// do something with the handler on top
+				
+				if (addNewItemOnline(item, handler)) {
+					// it's bad that user gets feedback eventhough sync is not run
+					sync();
+				}
+			}
+            
+            taskHandler.post(r);
+			
         } else {
 
             // other wise we are fine doing it in sync
@@ -68,8 +99,7 @@ public class ItemRepository {
             // notifications should come from the repo
             // about data change
 
-            // plus maybe some handler about success of the operation made
-
+			runOnUiThread(handler, false);	
             // catch exception let user know if was able to save
         }
 
@@ -77,24 +107,39 @@ public class ItemRepository {
 
     private void updateLocal() {
         ItemsApi api = itemsApiFactory.create();
-        api.getGtaskLastUpdate()
+		
+		// get 
+		
+		// should be processed for polymorphism
+		ItemList itemList = api.getItemListGVersion(localVersion);
+		
+		// save everything to here
+		
+		// update localVersion
     }
 
     private void updateRemote() {
-
+		// extract from repository everything that is IN-PROGRESS or LOCAL
+		// and in the same transaction mark it IN-PROGRESS
+		
+		// send it to server
+		
+		// for each we do add new. if new fails, item is already there and it's good
+		
+		// mark all items remote
     }
 
 
     // this is method is purely internet
-    public void sync(OutcomeHandler handler) {
+    public void sync(Handler handler) {
         updateLocal();
         updateRemote();
 
-        handler.handle(false);
+        runOnUiThread(handler, false);
     }
 
 
-    private boolean addNewItemOnline(Item item, OutcomeHandler handler) {
+    private boolean addNewItemOnline(Item item, Handler handler) {
         ItemsApi api = itemsApiFactory.create();
         // so we have to use fucking if statement and it will happen many many times.
         if (item.getItemKind() == ItemKind.Item) {
@@ -102,58 +147,80 @@ public class ItemRepository {
         } else {
             api.addTaskOnline(item).execute();
         }
-
+		runOnUiThread(handler, false);
         // in case of exception
         // can actually sort out exceptions to figure out if can do it offline
-        return false;
+
+		return false;
 
         return true;
     }
 
     public UUID getRootId() {
-        return null;
+        return dbRepo.getRootId();
     }
 
     public boolean hasRemovedItems() {
-    }
-
-    public void addItemList(List<Item> result) {
-    }
+		// offline
+	}
+	
+	// user has to handle
+    public void addItemList(List<Item> result, Handler handler) {
+		// try online.
+	}
 
     public Item getItem(UUID parentId) {
-    }
+		// offline
+	}
 
-    public void updateItem(Item moveItem) {
-    }
+    public void updateItem(Item moveItem) {    
+		// online first
+		// then try offline if local one
+	}
 
-    public void updateAllItems(List<Item> items) {
-    }
+	// items has to be onces from the server
+	// can we force server involvement???
+    private void updateAllItems(List<Item> items) {
+		// update is based on where you are.
+		// 
+	}
 
     public void deleteItem(Item item) {
-    }
+		// go online
+		// offline only if newly created
+	}
 
     public void deleteChildren(Item i) {
-    }
+		// go online only
+		// can accomplish by deleteItem one by one
+	}
 
     public void undoLastRemoval() {
-    }
+		// retrieve last successful removal and try to undo online
+		// else we unable to do anything about it.
+	}
 
     public int getChildrenCount(UUID parentId) {
+		// offline
     }
 
     public Collection<? extends Item> getChildren(UUID parentId) {
-        return null;
+        // offline
+		return null;
     }
 
-
-    void runOnUiThread(OutcomeHandler handler, boolean outcome) {
-
+	// check handler for null, add some description for user to post
+	// make Handler an optional type
+    void runOnUiThread(Handler handler, boolean outcome) {
+        taskHandler.post(new Runnable() {
+			void run() {
+				Message.obtainMsg;
+				msg.obj = outcome; // success or not
+				handler.postMessage(msg);
+			}
+		} 
     }
 
-
-    public interface OutcomeHandler {
-        void handle(boolean success);
-    }
 
     // conversion methods
 }
