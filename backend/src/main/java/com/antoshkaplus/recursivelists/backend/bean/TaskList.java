9ffe31757820b42239983cf59f162c850d33fd5f
Anton Logunov <antoshkaplus@gmail.com>
2018-01-14 01:25:43 -0500

    migration to app engine framework v2

--- backend/src/main/java/com/antoshkaplus/recursivelists/backend/bean/TaskList.java
+++ backend/src/main/java/com/antoshkaplus/recursivelists/backend/bean/TaskList.java
@@ -1,16 +1,9 @@
 package com.antoshkaplus.recursivelists.backend.bean;
 
-import com.antoshkaplus.recursivelists.backend.model.Item;
 import com.antoshkaplus.recursivelists.backend.model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import ch.lambdaj.function.convert.Converter;
-
-import static ch.lambdaj.Lambda.convert;
-import static ch.lambdaj.Lambda.extract;
-import static ch.lambdaj.Lambda.on;
 
 /**
  * Created by antoshkaplus on 8/4/17.
